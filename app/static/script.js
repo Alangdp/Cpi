492d5c0faa3599e540badd3562c110d8092b6ccd
ÁLan Gabriel Dias Pedon <alangabrieldias@hotmail.com>
2022-10-18 18:08:24 -0300

    geral commit

--- app/static/script.js
+++ app/static/script.js
@@ -1,12 +1,3 @@
-function httpGet(ticker)
-{
-    url = "/valid?ticker=" + ticker;
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); 
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
 function detalhes(){
     var ticker = prompt("Digite o ticker: ")
     if(!ticker ){
@@ -19,39 +10,5 @@ function detalhes(){
         alert('Ticker válido')
         location.href = `/detalhes?ticker=${ticker}`;
     }
-
 }
 
-function getTicker(){
-    var ticker = prompt("Digite o ticker: ")
-    if(!ticker ){
-        alert('Ticker inválido');
-    }if(ticker.length > 6){
-        alert('Ticker inválido');
-    }if(ticker.length < 5){
-        alert('Ticker inválido')
-    }else{
-        alert('Ticker válido')
-        location.href = 'https://statusinvest.com.br/acoes/{{ticker}}';
-    }
-
-}
-
-//////////////////////////////////////////////////
-
-const content = document.querySelector('#content');
-content.style.transformOrigin = 'top left';
-
-window.addEventListener('resize', recalculateScale);
-
-function recalculateScale() {
-  const windowHeight = window.innerHeight;
-  content.style.transform = `scale(${windowHeight / content.offsetHeight})`;
-}
-
-/////////////////////
-
-function Search(){
-    const Evento = document.getElementById('search').value;
-
-}
